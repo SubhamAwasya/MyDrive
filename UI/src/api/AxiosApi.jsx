72dde0285746184ef1b5683dcb8a4e4b9f604e54
Shubham Awasya <suawasya@gmail.com>
2025-07-25 14:55:05 +0530

    update server url

--- UI/src/api/AxiosApi.jsx
+++ UI/src/api/AxiosApi.jsx
@@ -1,8 +1,11 @@
 // axios.js
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
