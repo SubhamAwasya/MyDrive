925fecbb2a983f889e6fc634f7f3a7ff06bd4496
Shubham Awasya <suawasya@gmail.com>
2025-09-05 21:42:07 +0530

    turn on production mode

--- UI/src/api/AxiosApi.jsx
+++ UI/src/api/AxiosApi.jsx
@@ -1,7 +1,7 @@
 // axios.js
 import axios from "axios";
 
-const isProduction = false;
+const isProduction = true;
 export const serverURL = isProduction
   ? "https://mydrive-75kj.onrender.com"
   : "http://localhost:3000";
